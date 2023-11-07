f756b7296ac8f4212a6de58b2dc3696d523108f1
vnikitin <valerii.nikitin@qiio.com>
2023-11-07 14:29:14 +0200

    Added mock implementation of FS lib

---
+++ autumn_db/fs_lib/fs_lib/data_access/impl.py
@@ -0,0 +1,119 @@
+import datetime
+import json
+import os
+import shutil
+
+from fs_lib.data_access import FSOperations, ServiceOperations, CollectionMeta, FSCollectionService
+
+
+class FSCollectionMeta(CollectionMeta):
+
+    def __init__(self, collection_name: str):
+        super().__init__(collection_name)
+
+        meta_holder_name = '_metadata'
+        self._metadata_holder_path = os.path.join(collection_name, meta_holder_name)
+
+        data_holder_name = '_data'
+        self._data_holder_path = os.path.join(collection_name, data_holder_name)
+
+    @property
+    def path_to_metadata(self) -> str:
+        return self._metadata_holder_path
+
+    @property
+    def path_to_data(self) -> str:
+        return self._data_holder_path
+
+
+class FSServiceOperations(ServiceOperations):
+
+    def __init__(self, collection_meta: FSCollectionMeta):
+        super().__init__(collection_meta)
+        self._fs_collection_meta = collection_meta
+
+    def create_collection(self):
+        collection_name = self._collection_meta.collection_name
+        if os.path.exists(collection_name):
+            raise RuntimeError(f"Could not create collection {collection_name}. Collection already exists")
+
+        os.makedirs(self._fs_collection_meta.path_to_metadata)
+        os.makedirs(self._fs_collection_meta.path_to_data)
+
+    def remove_collection(self):
+        collection_name = self._collection_meta.collection_name
+        shutil.rmtree(collection_name)
+
+
+# TODO implement
+class FSOperationsImpl(FSOperations):
+    pass
+
+
+class FSOperationsMockImpl(FSOperations):
+    UPDATED_AT_KEY = 'updated_at'
+    IS_FROZEN_KEY = 'is_frozen'
+
+    def __init__(self, fs_collection_meta: FSCollectionMeta):
+        super().__init__(fs_collection_meta)
+        self._fs_collection_meta: FSCollectionMeta = fs_collection_meta
+
+    def _prepare_path(self, filename: str, is_metadata: bool = False):
+        full_filename = f"{filename}{self._filename_ending}"
+        holder = self._fs_collection_meta.path_to_metadata if is_metadata else self._fs_collection_meta.path_to_data
+        path = os.path.join(holder, full_filename)
+
+        return path
+
+    def create(self, filename: str, data: str):
+        data_path = self._prepare_path(filename)
+        metadata_path = self._prepare_path(filename, True)
+
+        for path in [data_path, metadata_path]:
+            if os.path.exists(path):
+                raise RuntimeError(f"File {path} already exists")
+
+        with open(data_path, 'w') as f:
+            f.write(data)
+        with open(metadata_path, 'w') as f:
+            metadata = {
+                FSOperationsMockImpl.UPDATED_AT_KEY: datetime.datetime.utcnow().strftime(FSCollectionService.UTC_FORMAT),
+                FSOperationsMockImpl.IS_FROZEN_KEY: False
+            }
+            _metadata = json.dumps(metadata)
+            f.write(_metadata)
+
+    def update(self, filename: str, data: str):
+        data_path = self._prepare_path(filename)
+        metadata_path = self._prepare_path(filename, True)
+
+        for path in [data_path, metadata_path]:
+            if not os.path.exists(path):
+                raise RuntimeError(f"Could not update {path}. File does not exist")
+
+        with open(data_path, 'w') as f:
+            f.write(data)
+        with open(metadata_path, 'r+') as f:
+            _metadata = f.read()
+            metadata = json.loads(_metadata)
+            metadata[FSOperationsMockImpl.UPDATED_AT_KEY] = datetime.datetime.utcnow().strftime(FSCollectionService.UTC_FORMAT)
+            _metadata = json.dumps(metadata)
+            f.write(_metadata)
+
+    def read(self, filename: str) -> str:
+        path = self._prepare_path(filename)
+
+        if not os.path.exists(path):
+            raise RuntimeError(f"Could not read {path}. File does not exist")
+
+        with open(path, 'r') as f:
+            data = f.read()
+
+        return data
+
+    def delete(self, filename: str):
+        data_path = self._prepare_path(filename)
+        metadata_path = self._prepare_path(filename, True)
+
+        os.remove(data_path)
+        os.remove(metadata_path)
---
+++ autumn_db/fs_lib/fs_lib/examples/basic_operations.py
@@ -0,0 +1,17 @@
+from fs_lib.data_access.impl import FSOperationsMockImpl, FSCollectionMeta, FSServiceOperations
+
+fs_col_meta = FSCollectionMeta('my_collection')
+
+fs_service_opers = FSServiceOperations(fs_col_meta)
+fs_service_opers.create_collection()
+
+fs_opers = FSOperationsMockImpl(fs_col_meta)
+fs_opers.create('123', '456')
+fs_opers.update('123', '789')
+
+content = fs_opers.read('123')
+print(content)
+
+fs_opers.delete('123')
+
+fs_service_opers.remove_collection()
